9dc822da3fc615b745c97b12a4d04e8af360afe6
eloybugha <erroldeluterrol@gmail.com>
2026-04-30 16:08:04 +0200

    Update ga.js

--- assets/ga.js
+++ assets/ga.js
@@ -1,4 +1,7 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-KCKEVDK6RY');
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KCKEVDK6RY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-KCKEVDK6RY');
+</script>
